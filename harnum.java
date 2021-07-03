7393d9e2197de078d3b513385be1e5907fda92c5
sknithin06 <nithinsk939@gmail.com>
2021-07-04 00:46:07 +0530

    number is added

--- harnum.java
+++ harnum.java
@@ -1,6 +1,19 @@
 package Java;
+import java.util.Scanner;
 public class harnum {
 	public static void main(String[] args) {
 		System.out.println("Welcome to BasicCoreProgram");
+		
+		Scanner sc= new Scanner(System.in);   
+		System.out.print("Enter a number:");  
+		int n=sc.nextInt(); 
+		
+
+		double sum = 0.0;
+		for (int i = 1; i <= n; i++) {
+			sum = sum + (double) 1 / i;
+			System.out.println("Harmonic value of given number is:"+sum);
+		}
+		System.out.println("Harmonic value of given number is:"+sum);
 	}
 }
